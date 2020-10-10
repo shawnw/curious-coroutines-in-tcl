e88cee933aa1fb6f94f9a38ee204a9612a22ada3
Shawn Wagner <shawnw.mobile@gmail.com>
2020-10-10 12:00:21 -0700

    Add cofollow.tcl example

---
+++ coroutines/cofollow.tcl
@@ -0,0 +1,35 @@
+#!/usr/bin/env tclsh
+package require Tcl 8.6
+
+# A simple example showing how to hook up a pipeline with
+# coroutines. To run this, you will need a log file.  Run the program
+# logsim.py in the background to get a data source.
+
+# A data source.  This is not a coroutine, but it sends data into one
+# (target)
+
+proc follow {thefile target} {
+    seek $thefile 0 end
+    while 1 {
+        set len [gets $thefile line]
+        if {$len < 0} {
+            after 100
+            continue
+        }
+        $target $line
+    }
+}
+
+# A sink. A coroutine that receives data
+
+# Use a lambda expression instead of an existing proc for the
+# coroutine
+coroutine printer apply {{} {
+    while 1 {
+        set line [yield]
+        puts $line
+    }
+}}
+
+set f [open access-log]
+follow $f printer
