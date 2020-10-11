2038c69fd152d688d49b1e7ff0f020d185b780cb
Shawn Wagner <shawnw.mobile@gmail.com>
2020-10-10 22:55:44 -0700

    Add coexpat.tcl example.

---
+++ coroutines/coexpat.tcl
@@ -0,0 +1,6 @@
+#!/usr/bin/env tclsh
+
+# The python "low level" interface to a XML parser is the same as tcl's
+# only level. Unless there's an OO version of tDOM floating around somewhere...
+
+puts "See cosax.tcl"
