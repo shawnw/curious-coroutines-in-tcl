226ad75878e1239fa2391d0b370527995d2372f6
Shawn Wagner <shawnw.mobile@gmail.com>
2020-10-11 02:53:48 -0700

    Add tclos6.tcl example

---
+++ coroutines/tclos6.tcl
@@ -0,0 +1,245 @@
+#!/usr/bin/env tclsh
+package require Tcl 8.6
+package require TclOO
+
+# ------------------------------------------------------------
+# tclos6.tcl  -  The TCL Operating System
+#
+# Added support for task waiting
+# ------------------------------------------------------------
+
+# ------------------------------------------------------------
+#                       === Tasks ===
+# ------------------------------------------------------------
+# From https://wiki.tcl-lang.org/page/TclOO+Tricks
+oo::class create Static {
+    method static {args} {
+        if {![llength $args]} return
+        set callclass [lindex [self caller] 0]
+        oo::objdefine $callclass export varname
+        foreach vname $args {
+            lappend pairs [$callclass varname $vname] $vname
+        }
+        uplevel 1 upvar {*}$pairs
+    }
+}
+
+# This object encapsulates a running task.
+oo::class create Task {
+    mixin Static
+    variable sendval target tid
+    
+    constructor {target_} {
+        my static taskid
+        set tid [incr taskid]
+        set target $target_
+        set sendval ""
+    }
+
+    destructor {
+        if {[info commands $target] ne ""} {
+            rename $target ""
+        }
+    }
+    
+    # Run a task until it hits the next yield statement
+    method run {} {
+        $target $sendval
+    }
+    
+    method gettid {} {
+        return $tid
+    }
+
+    method gettarget {} {
+        return $target
+    }
+    
+    method setsendval {sendval_} {
+        set sendval $sendval_
+    }
+}
+
+# ------------------------------------------------------------
+#                      === Scheduler ===
+# ------------------------------------------------------------
+oo::class create Scheduler {
+    variable exit_waiting ready taskmap
+
+    constructor {} {
+        set exit_waiting [dict create]
+        set ready [list]
+        set taskmap [dict create]
+    }
+    
+    method add {target} { ;# Called new in pyos5.py
+        set newtask [Task new $target]
+        set tid [$newtask gettid]
+        dict set taskmap $tid $newtask
+        my schedule $newtask
+        return $tid
+    }
+
+    method gettask {tid} {
+        if {[dict exists $taskmap $tid]} {
+            return [dict get $taskmap $tid]
+        } else {
+            return ""
+        }
+    }
+    
+    method exit {task} {
+        set tid [$task gettid]
+        puts "Task $tid terminated"
+        dict unset taskmap $tid
+        if {[dict exists $exit_waiting $tid]} {
+            foreach watcher [dict get $exit_waiting $tid] {
+                my schedule $watcher
+            }
+            dict unset exit_waiting $tid
+        }
+        $task destroy
+    }
+
+    method waitforexit {task waittid} {
+        if {[dict exists $taskmap $waittid]} {
+            dict lappend exit_waiting $waittid $task
+            return true
+        } else {
+            return false
+        }
+    }
+
+    method schedule {task} {
+        lappend ready $task
+    }
+    
+    method mainloop {} {
+        while {[dict size $taskmap] > 0} {
+            set ready [lassign $ready task]
+            try {
+                set result [$task run]
+                if {[info object isa typeof $result SystemCall]} {
+                    $result settask $task
+                    $result setsched [self object]
+                    $result handle
+                    $result destroy
+                } else {
+                    my schedule $task
+                }
+            } trap {TCL LOOKUP COMMAND} {} {
+                my exit $task
+            }
+        }
+    }
+}
+
+# ------------------------------------------------------------
+#                   === System Calls ===
+# ------------------------------------------------------------
+oo::class create SystemCall {
+    variable sched task
+    method handle {} {}
+    method settask {task_} {
+        set task $task_
+    }
+    method setsched {sched_} {
+        set sched $sched_
+    }
+}
+
+# Return a task's ID number
+oo::class create GetTid {
+    superclass SystemCall
+    variable sched task
+    method handle {} {
+        $task setsendval [$task gettid]
+        $sched schedule $task
+    }
+}   
+
+# Create a new task
+oo::class create NewTask {
+    superclass SystemCall
+    variable sched target task
+
+    constructor {target_} {
+        set target $target_
+    }
+
+    method handle {} {
+        set tid [$sched add $target]
+        $task setsendval $tid
+        $sched schedule $task
+    }
+}
+
+# Kill a task
+oo::class create KillTask {
+    superclass SystemCall
+    variable sched task tid
+
+    constructor {tid_} {
+        set tid $tid_
+    }
+
+    method handle {} {
+        set ktask [$sched gettask $tid]
+        if {$ktask ne ""} {
+            $ktask destroy
+            $task setsendval true
+        } else {
+            $task setsendval false
+        }
+        $sched schedule $task
+    }
+}
+
+# Wait for a task to exit
+oo::class create WaitTask {
+    superclass SystemCall
+    variable sched task tid
+
+    constructor {tid_} {
+        set tid $tid_
+    }
+
+    method handle {} {
+        set result [$sched waitforexit $task $tid]
+        $task setsendval $result
+        if {!$result} {
+            $sched schedule $task
+        }
+    }
+}
+
+# ------------------------------------------------------------
+#                      === Example ===
+# ------------------------------------------------------------
+
+proc foo {} {
+    variable counter
+    coroutine foo[incr counter] apply {{} {
+        yield [info coroutine]
+        for {set i 1} {$i <= 5} {incr i} {
+            puts "I'm foo"
+            yield
+        }
+    }}
+}
+
+proc main {} {
+    variable counter
+    coroutine main[incr counter] apply {{} {
+        yield [info coroutine]
+        set childtask [NewTask new [foo]]
+        set child [yield $childtask]
+        puts "Waiting for child $child"
+        yield [WaitTask new $child]
+        puts "Child done"
+    }}
+}
+
+Scheduler create sched
+sched add [main]
+sched mainloop
