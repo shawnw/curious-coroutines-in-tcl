20364039b1eed41be3337808660ec3a8f1839968
Shawn Wagner <shawnw.mobile@gmail.com>
2020-10-16 21:14:28 -0700

    Updated blaster.tcl

--- coroutines/blaster.tcl
+++ coroutines/blaster.tcl
@@ -19,7 +19,7 @@ set NCONNECTIONS 300 ;# Number of connections to make
 set MSGSIZE 1024 ;# Message size
 set SERVER {localhost 45000} ;# Server address
 set MSG [string repeat x $MSGSIZE] ;# The message
-set MAXCOUNT 1000000 ;# The number of messages to send
+set MAXCOUNT 100000 ;# The number of messages to send
 
 generator define range {n m} {
     for {set i $n} {$i <= $m} {incr i} { generator yield $i }
@@ -45,27 +45,39 @@ proc send_message {s} {
     global MAXCOUNT
     global count
     global forever
+    global sent_bytes
     incr count
     puts "Sending message $count to socket $s"
     try {
-        chan puts $s $MSG
+        chan puts -nonewline $s $MSG
+        dict incr sent_bytes $s [string length $MSG]
     } trap error {msg} {
         puts stderr "Socket $s error on writing: $msg"
         shutdown $s
     } finally {
         if {$count == $MAXCOUNT} {
-            set forever done
+            global connections
+            dict for {so _} $connections {
+                chan event $so writable {}
+            }
         }
     }
 }
 
 proc read_replies {s} {
+    global sent_bytes
     try {
         set r [chan read $s 65536]
-        set len [string length $r]
-        puts "Reading $len bytes from socket $s"
-        if {$len == 0} {
+        if {[eof $s]} {
             shutdown $s
+        } else {
+            set len [string length $r]
+            puts "Reading $len bytes from socket $s"
+            dict incr sent_bytes $s [expr {-$len}]
+            if {[llength [chan event $s writable]] == 0
+                && [dict get $sent_bytes $s] == 0} {
+                shutdown $s
+            }
         }
     } trap error {msg} {
         puts stderr "Socket $s error on reading: $msg"
@@ -78,7 +90,7 @@ generator foreach i [range 1 $NCONNECTIONS] {
     set s [socket {*}$SERVER]
     dict incr connections $s
     # Install handlers
-    chan configure $s -blocking 0
+    chan configure $s -blocking 0 -buffering none
     chan event $s writable [list send_message $s]
     chan event $s readable [list read_replies $s]
 }
